41959fc7bcd5f8a52148b80e464b138d958437a4
Aaron W. West <tallpeak@hotmail.com>
2023-09-01 18:52:18 -0700

    textareas,fix labels,update upon every keystroke (oninput)

--- morse.js
+++ morse.js
@@ -1,68 +1,11 @@
 // ASCII 32 to 95, where "" means undefined
-const morse = [" ",
-    "-.-.--",
-    ".-..-.",
-    "",
-    "...-..-",
-    "",
-    ".-...",
-    ".----.",
-    "-.--.",
-    "-.--.-",
-    "",
-    ".-.-.",
-    "--..--",
-    "-....-",
-    ".-.-.-",
-    "-..-.",
-    "-----",
-    ".----",
-    "..---",
-    "...--",
-    "....-",
-    ".....",
-    "-....",
-    "--...",
-    "---..",
-    "----.",
-    "---...",
-    "-.-.-.",
-    "",
-    "-...-",
-    "",
-    "..--..",
-    ".--.-.",
-    ".-",
-    "-...",
-    "-.-.",
-    "-..",
-    ".",
-    "..-.",
-    "--.",
-    "....",
-    "..",
-    ".---",
-    "-.-",
-    ".-..",
-    "--",
-    "-.",
-    "---",
-    ".--.",
-    "--.-",
-    ".-.",
-    "...",
-    "-",
-    "..-",
-    "...-",
-    ".--",
-    "-..-",
-    "-.--",
-    "--..",
-    "",
-    "",
-    "",
-    "",
-    "..--.-"]
+const morse = [ " ", "-.-.--", ".-..-.", "", "...-..-", "", ".-...", ".----.", "-.--.", "-.--.-",
+                "", ".-.-.", "--..--", "-....-", ".-.-.-", "-..-.", "-----", ".----", "..---", "...--",
+                "....-", ".....", "-....", "--...", "---..", "----.", "---...", "-.-.-.", "", "-...-",
+                "", "..--..", ".--.-.", ".-", "-...", "-.-.", "-..", ".", "..-.", "--.",
+                "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-",
+                ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", "",
+                "", "", "", "..--.-" ]
 
 var morse_to_char = {}
 
@@ -74,39 +17,40 @@ var morse_to_char = {}
 //     }
 // }
 
-function set_morse_to_char(morse,char){ 
-    morse_to_char[morse] = String.fromCharCode(char+32);
+//const input = document.querySelector("input");
+const input = document.getElementById("input")
+input.addEventListener("input", input_changed)
+
+function set_morse_to_char(morse, char) {
+    morse_to_char[morse] = String.fromCharCode(char + 32)
 }
 
 function init_reverse() {
     morse.forEach(set_morse_to_char)
 }
 
-/**
- * The event handler triggered when opening the spreadsheet.
- * @param {Event} e The onOpen event.
- * @see https://developers.google.com/apps-script/guides/triggers#onopene
- */
-function onOpen(e) {
-  init_reverse() 
-}
-
 function morse_decode(morse) {
     let s = ""
-    for (let code of morse.split(" ") ) {
-        s += morse_to_char[code]
+    for (let code of morse.split(" ")) {
+        if (code.length) {
+            let m = morse_to_char[code]
+            if (m) s+=m
+        }
     }
     return s
 }
 
-init_reverse() 
+init_reverse()
 
-function convert_morse_to_ascii(e)
-{
+function convert_morse_to_ascii(e) {
     var el_in = document.getElementById("input")
     var el_out = document.getElementById("output")
-    //el_out.textContent = el_in.value
     el_out.textContent = morse_decode(el_in.value)
+}
+
+function input_changed(e) { 
+    var el_out = document.getElementById("output")
+    el_out.textContent = morse_decode(e.target.value);
 }
 
 //console.log(morse_decode("... --- ...")) // SOS
