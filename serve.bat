b17737335dd7da62244c2349aaf02dba9fc3e32e
Aaron W. West <tallpeak@hotmail.com>
2023-09-03 10:17:21 -0700

    debug html server using python

---
+++ serve.bat
@@ -0,0 +1,8 @@
+@echo off
+echo This starts a debug server
+echo The start command asynchronously starts your default web browser
+echo Note Port 7767 = MC (ASCII codes), for Morse Code
+start "debug browser" "http://localhost:7767"
+python -m http.server 7767
+@echo if the last command didn't work, then run:
+@echo pip install simple-http-server
